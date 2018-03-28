506b0c0b25e8390a2f6a68c60bf0c193d641081d
Federico Ficarelli <federico.ficarelli@gmail.com>
2018-03-28 14:40:41 +0200

    Add slurm error handling API

--- slurm/cgo_helpers.h
+++ slurm/cgo_helpers.h
@@ -1,4 +1,4 @@
-// WARNING: This file has automatically been generated on Wed, 28 Mar 2018 14:32:22 CEST.
+// WARNING: This file has automatically been generated on Wed, 28 Mar 2018 14:39:52 CEST.
 // By https://git.io/c-for-go. DO NOT EDIT.
 
 #include <slurm/slurm.h>
--- slurm/slurm.go
+++ slurm/slurm.go
@@ -1,4 +1,4 @@
-// WARNING: This file has automatically been generated on Wed, 28 Mar 2018 14:32:22 CEST.
+// WARNING: This file has automatically been generated on Wed, 28 Mar 2018 14:39:52 CEST.
 // By https://git.io/c-for-go. DO NOT EDIT.
 
 package slurm
@@ -17,6 +17,21 @@ func SubmitBatchJob(jobDescMsg *JobDescMsg, slurmAllocMsg **SubmitResponseMsg) i
 	cjobDescMsg, _ := jobDescMsg.PassRef()
 	cslurmAllocMsg, _ := (**C.submit_response_msg_t)(unsafe.Pointer(slurmAllocMsg)), cgoAllocsUnknown
 	__ret := C.slurm_submit_batch_job(cjobDescMsg, cslurmAllocMsg)
+	__v := (int32)(__ret)
+	return __v
+}
+
+// Strerror function as declared in slurm/slurm_errno.h:290
+func Strerror(errnum int32) *byte {
+	cerrnum, _ := (C.int)(errnum), cgoAllocsUnknown
+	__ret := C.slurm_strerror(cerrnum)
+	__v := *(**byte)(unsafe.Pointer(&__ret))
+	return __v
+}
+
+// GetErrno function as declared in slurm/slurm_errno.h:296
+func GetErrno() int32 {
+	__ret := C.slurm_get_errno()
 	__v := (int32)(__ret)
 	return __v
 }
