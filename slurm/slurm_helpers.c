ddd0a7a05e2bda91b773b3d6a6950cbc26539475
Federico Ficarelli <federico.ficarelli@gmail.com>
2018-03-30 12:28:26 +0200

    Remove redundant comments

--- slurm/slurm_helpers.c
+++ slurm/slurm_helpers.c
@@ -4,9 +4,6 @@
 submit_response_msg_t* wrap_slurm_submit_batch_job(job_desc_msg_t *job_desc_msg) {
     submit_response_msg_t* response;
     if (slurm_submit_batch_job(job_desc_msg, &response) != 0) {
-        // API docs don't say nothing about the state of the
-        // out pointer in case of failure. Let's free it and
-        // return NULL just in case.
         slurm_free_submit_response_response_msg(response);
         response = NULL;
     }
@@ -16,9 +13,6 @@ submit_response_msg_t* wrap_slurm_submit_batch_job(job_desc_msg_t *job_desc_msg)
 job_info_msg_t* wrap_slurm_load_job(uint32_t job_id, uint16_t show_flags) {
     job_info_msg_t* response;
     if (slurm_load_job(&response, job_id, show_flags) != 0) {
-        // API docs don't say nothing about the state of the
-        // out pointer in case of failure. Let's free it and
-        // return NULL just in case.
         slurm_free_job_info_msg(response);
         response = NULL;
     }
