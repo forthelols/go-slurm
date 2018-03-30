3615513c3b87fc4cb6af204b50326e3b28934641
Federico Ficarelli <federico.ficarelli@gmail.com>
2018-03-30 11:20:24 +0200

    Add docs to API helpers

--- slurm/slurm_helpers.c
+++ slurm/slurm_helpers.c
@@ -4,9 +4,9 @@
 submit_response_msg_t* wrap_slurm_submit_batch_job(job_desc_msg_t *job_desc_msg) {
     submit_response_msg_t* response;
     if (slurm_submit_batch_job(job_desc_msg, &response) != 0) {
-        // API doesn't say nothing about the state of
-        // out pointer in case of failure. Let's free
-        // it and return NULL just in case.
+        // API docs don't say nothing about the state of the
+        // out pointer in case of failure. Let's free it and
+        // return NULL just in case.
         slurm_free_submit_response_response_msg(response);
         response = NULL;
     }
--- slurm/slurm_helpers.h
+++ slurm/slurm_helpers.h
@@ -1,5 +1,13 @@
 #pragma once
 #include <slurm/slurm.h>
 
+/*
+ * wrap_slurm_submit_batch_job - issue RPC to submit a job for later execution
+ * NOTE: free the response using slurm_free_submit_response_response_msg
+ * IN job_desc_msg - description of batch job request
+ * RET a pointer to a valid submit_response_msg_t allocated by the underlying
+ * call to slurm_submit_batch_job, otherwise return NULL and set errno to
+ * indicate the error
+ */
 extern submit_response_msg_t* wrap_slurm_submit_batch_job(job_desc_msg_t *job_desc_msg);
 
